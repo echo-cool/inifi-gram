5d8a0211668992c2f34c18adc87f04b85ba84a65
Yuyang Wang <yuyang.wang821@gmail.com>
2024-02-29 21:14:58 -0500

    Updated: Thu Feb 29 21:14:58 EST 2024

--- test.py
+++ test.py
@@ -7,8 +7,6 @@
 """
 import pickle
 
-for i in range(1, 21):
-    with open(f"n-gram/inv_term_{i}.pkl", "rb") as f:
-        inv_term = pickle.load(f)
-        print(f"n-gram number: {i}, inv_term length: {len(inv_term)}")
+data = pickle.load(open("query/inv_sentence.pkl", "rb"))
 
+print(data)
