ee08e63ba9ca495b0c4c3d8fd08d32ef5f7d001a
Yuyang Wang <yuyang.wang821@gmail.com>
2024-03-06 20:07:43 -0500

    Updated: Wed Mar  6 20:07:42 EST 2024

---
+++ inf-1.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2/29/24 17:01
+@Auth ： Wang Yuyang
+@File ：inf.py
+@IDE ：PyCharm
+"""
+import asyncio
+import os
+
+import pandas as pd
+from datasets import load_from_disk
+from tqdm import tqdm
+
+URL = "https://api.infini-gram.io/"
+dataset = load_from_disk("snli_with_id")
+corpus_name = "v4_dolmasample_olmo"
+
+output_file_name_csv = f"inf-gram/res-{corpus_name}.csv"
+output_file_name_parquet = f"inf-gram/res-{corpus_name}.parquet"
+
+import aiohttp  # Make sure to import aiohttp
+
+
+async def get_inf_gram_count(n_gram_data):
+    payload = {
+        "corpus": corpus_name,
+        "query_type": "count",
+        "query": n_gram_data,
+    }
+
+    headers = {"Content-Type": "application/json"}
+
+    # Using aiohttp.ClientSession for making asynchronous HTTP requests
+    async with aiohttp.ClientSession() as session:
+        try:
+            # Sending the POST request asynchronously
+            async with session.post(URL, json=payload, headers=headers, timeout=10) as response:
+
+                # Parsing the JSON response asynchronously
+                json_response = await response.json()
+
+                if "count" not in json_response:
+                    print("ERROR!")
+                    print(json_response)
+                    return -1
+
+                count = json_response["count"]
+                return count
+        except Exception as e:
+            print(e)
+            return -1
+
+
+async def main():
+    tqdm_instance = tqdm(dataset)
+    data = {}
+    if not os.path.exists("inf-gram"):
+        os.makedirs("inf-gram")
+    for index, example in enumerate(tqdm_instance):
+        premise = example["premise"]
+        hypothesis = example["hypothesis"]
+        doc_id = example["id"]
+        label = example["label"]
+
+        premise_count = await get_inf_gram_count(premise)
+
+        hypothesis_count = await get_inf_gram_count(hypothesis)
+
+        tqdm_instance.set_postfix(
+            doc_id=doc_id,
+            premise_count=premise_count,
+            hypothesis_count=hypothesis_count,
+        )
+
+        data[doc_id] = {
+            'doc_id': doc_id,
+            "premise": premise,
+            "hypothesis": hypothesis,
+            "label": label,
+            "premise_count": premise_count,
+            "hypothesis_count": hypothesis_count
+        }
+
+        if index % 100 == 0:
+            df = pd.DataFrame.from_dict(data, orient='index')
+            df.to_csv(output_file_name_csv)
+            df.to_parquet(output_file_name_parquet)
+
+    df = pd.DataFrame.from_dict(data, orient='index')
+    df.to_csv(output_file_name_csv)
+    df.to_parquet(output_file_name_parquet)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
